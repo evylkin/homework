93bd7175b5c445b7607e65ffcf72397baa0e5072
evylkin <mylt432@gmail.com>
2024-12-11 16:14:36 +0300

    Add files via upload

---
+++ 81 урок/main.js
@@ -0,0 +1,14 @@
+let numbers = [1,4,7,8,9,12]
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 != 0){
+        console.log(numbers[i])
+    }
+}
+
+// let colorRainbow = ['красный','оранжевый','желтый','зеленый','голубой','синий', 'фиолетовый']
+
+// for(let i = colorRainbow.length - 1 ; i >= 0; i--){
+//     console.log(colorRainbow[i])
+// }
+
