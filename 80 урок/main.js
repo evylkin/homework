9e41903bc2484147003375fa0c65bb7c9b610194
evylkin <mylt432@gmail.com>
2024-12-11 16:08:57 +0300

    Add files via upload

---
+++ 80 урок/main.js
@@ -0,0 +1,22 @@
+// for(let i = 2; i<=12; i = i+2){
+//     console.log(i)
+
+// }
+
+// for(let i = 10; i >=1; i--){
+//     console.log(i)
+// }
+
+// let i = 0
+
+// while(i <12){
+//     i = i+2
+//     console.log(i)
+// }
+
+// let i = 11
+
+// while( i>1){
+//     i--
+//     console.log(i)
+// }
