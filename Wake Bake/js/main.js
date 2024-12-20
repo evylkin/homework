5e4bf20ce7201e859c19c5cc0ba243d5fed9c7c1
evylkin <mylt432@gmail.com>
2024-12-20 21:32:25 +0300

    Add files via upload

--- Wake Bake/js/main.js
+++ Wake Bake/js/main.js
@@ -1,72 +1,104 @@
-(function(){
+(function () {
+    // бургер
+    document.addEventListener('click', burgerInt)
 
-document.addEventListener('click',burgerInt)
+    function burgerInt(e) {
+        const burgerIcon = e.target.closest('.burger-icon')
 
-function burgerInt(e){
-const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.list__link')
 
-const burgerNavLink = e.target.closest('.list__link')
-
-if(! burgerIcon && !burgerNavLink) return
-if(document.documentElement.clientWidth > 900)return
-
-
-if(!document.body.classList.contains('body--open-menu') ){
-    document.body.classList.add('body--open-menu')
-} else{
-    document.body.classList.remove('body--open-menu')
-}
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
 
-
-}
-
-const modal = document.querySelector('.modal')
-const modalBtn = document.querySelector('.about__img-button')
-
-
-modalBtn.addEventListener('click', openModal)
-modal.addEventListener('click', closeModal)
-
-function openModal(e){
-    e.preventDefault()
-    document.body.classList.toggle('body--open-modal')
-}
-function closeModal(e){
-    e.preventDefault()
+        if (!document.body.classList.contains('body--open-menu')) {
+            document.body.classList.add('body--open-menu')
+        } else {
+            document.body.classList.remove('body--open-menu')
+        }
 
 
-    const target = e.target
-    if(target.closest('.modal__cancel')|| (target.classList.contains('modal'))){
-        document.body.classList.remove('body--open-modal')
+
     }
-}
 
-const tabControls = document.querySelector('.tab-controls')
-
-tabControls.addEventListener('click', toggleTub)
-
-
-function toggleTub(e) {
-
-    const tabControl = e.target.closest('.tab-controls__link')
-
-    if(!tabControl) return
-
-    e.preventDefault()
-
-    if(tabControl.classList.contains('tab-controls__item--active')) return
-
-    const tabContentID = tabControl.getAttribute('href')
-    document.querySelector('.tab-content--show').classList.remove('tab-content--show')
-    document.querySelector(tabContentID).classList.add('tab-content--show')
-
-document.querySelector('.tab-controls__item--active').classList.remove('tab-controls__item--active')
-    tabControl.classList.add('tab-controls__item--active')
-    
-}
+    // модалка
+    const modal = document.querySelector('.modal')
+    const modalBtn = document.querySelector('.about__img-button')
 
 
+    modalBtn.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--open-modal')
+    }
+    function closeModal(e) {
+        e.preventDefault()
+
+
+        const target = e.target
+        if (target.closest('.modal__cancel') || (target.classList.contains('modal'))) {
+            document.body.classList.remove('body--open-modal')
+        }
+    }
+
+    // табы
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTub)
+
+
+    function toggleTub(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+
+        e.preventDefault()
+
+        if (tabControl.classList.contains('tab-controls__item--active')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        document.querySelector('.tab-content--show').classList.remove('tab-content--show')
+        document.querySelector(tabContentID).classList.add('tab-content--show')
+
+        document.querySelector('.tab-controls__item--active').classList.remove('tab-controls__item--active')
+        tabControl.classList.add('tab-controls__item--active')
+
+    }
+
+    // Аккордеон
+
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+        })
+    })
 
 })()
 
