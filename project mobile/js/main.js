1cfcb10dbe40b60342c77830bb427fc45b98cf58
evylkin <mylt432@gmail.com>
2025-01-10 16:55:21 +0300

    Add files via upload

--- project mobile/js/main.js
+++ project mobile/js/main.js
@@ -1,29 +1,50 @@
 // аккардеон
 document.querySelectorAll('.accordion-list__control').forEach((e) => {
-    
+
     e.addEventListener('click', () => {
         let content = e.nextElementSibling;
 
-        if(content.style.maxHeight){
-            document.querySelectorAll('.accordion-list__content').forEach((e) => 
-                e.style.maxHeight = null)
-        } 
-        
-        else{
+        if (content.style.maxHeight) {
             document.querySelectorAll('.accordion-list__content').forEach((e) =>
-                e.style.maxHeight = null ) 
+                e.style.maxHeight = null)
+        }
+
+        else {
+            document.querySelectorAll('.accordion-list__content').forEach((e) =>
+                e.style.maxHeight = null)
             content.style.maxHeight = content.scrollHeight + 'px'
         }
     })
     // e.preventDefault()
 }
 )
-document.querySelector('.accordion-list__control').addEventListener('click', function(){
+document.querySelector('.accordion-list__control').addEventListener('click', function () {
     document.querySelector('.accordion-list__control').classList.toggle('accordion-list__item--opened')
 })
 
 // бургер
 
-document.getElementById('burger').addEventListener('click', function(){
+document.getElementById('burger').addEventListener('click', function () {
     document.querySelector('.body').classList.toggle('body--oppened-menu')
 })
+// слайдер
+new Swiper('.hero__slider', {
+
+    spaceBetween: 0,
+    slidesPerView: 1,
+
+
+    navigation: {
+        nextEl: '.slider__button-next',
+        prevEl: '.slider__button-prev',
+    },
+    pagination: {
+        el: '.hero__pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+});
